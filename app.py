5910450343cc911b0960c436403bdff530f5f29e
Ваше Имя <duni.b2003@gmail.com>
2025-07-24 23:34:55 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+# sexs = input("name: ")
+# s = "dunebek"
+# print(sexs)
+number = 21
+string = "duni"
+
+# 1 - int
+# 1.1 - float
+# "string" - str
+# True, False - bool
+
+# {} - dict
+# [] - list
+
+fruits1 = list("apple", "orange","limon", "banana") # первый способ создания списка
+fruits2 = ["apple","orange","limon", "banana"] # второй способ создания списка
+print(fruits2)
+
+# print(fruist[0]) # [] - index
+# print(fruist[1])
+# print(fruist[2])
+
+
+print(fruits2[-1])
+
+# print(type(number))
+
+g = [
+    [
+        21,
+        45,
+
+    ]    
+]
+print(type(g))
+
+d = {
+    "key": 'value',
+    "name": "dunebek",
+}
+
+print(d["key"])
+print(list(d.keys()))
